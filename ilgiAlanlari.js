ad3b44b584a1209dd0c50a83f6a6cd305fb4c478
AydinIlyas <100437523+AydinIlyas@users.noreply.github.com>
2022-05-15 23:51:16 +0300

    Delete ilgiAlanlari.js

--- ilgiAlanlari.js
+++
@@ -1,9 +0,0 @@
-async function ilyas() {
-    const response = await fetch("https://www.breakingbadapi.com/api/characters");
-    const dataCollect = await response.json();
-
-    console.log(dataCollect);
-
-    document.querySelector("#resim").src = dataCollect[0].img;
-}
-ilyas();
